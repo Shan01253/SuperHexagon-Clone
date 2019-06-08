35021ad265e13c09ebf685d5a13cbf3280b5baf9
Sha Anwar <Shan01253@gmail.com>
2019-06-08 16:19:08 -0400

    First Commit
    Added Unity Project to Github. Probably the last Commit

---
+++ SuperHexagon Attempt 1/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject panelUI;
+    public TextMeshProUGUI scoreText;
+    private int score;
+    private int timer;
+
+    private void Start()
+    {
+        panelUI.SetActive(false);
+        score = 0;
+        scoreText.text = "SCORE: " + score;
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        timer++;
+        CanvasElement();
+        if (panelUI.activeInHierarchy == false)
+        {
+            UpdateScore();
+        }
+         //UpdateScore();
+    }
+
+    public void UpdateScore()
+    {
+        if (timer >= 100)
+        {
+            score += 10;
+            scoreText.text = "SCORE: " + score.ToString();
+            timer = 0;
+        }
+    }
+
+    public void CanvasElement()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            panelUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+        panelUI.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
+    }
+}
---
+++ SuperHexagon Attempt 1/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public float movementSpeed = 600f;
+
+	float movement = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		movement = Input.GetAxisRaw("Horizontal");
+
+	}
+
+	public void FixedUpdate(){
+		transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -movementSpeed);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision){
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene("GameOver");
+	}
+}
